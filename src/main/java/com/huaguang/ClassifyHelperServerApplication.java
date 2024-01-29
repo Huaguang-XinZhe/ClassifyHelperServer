099c09b31a1197d345a1164d8a640158d0a3bbb5
HuaGuang <2475096613@qq.com>
2024-01-30 01:10:58 +0800

    First Commit!

---
+++ src/main/java/com/huaguang/ClassifyHelperServerApplication.java
@@ -0,0 +1,13 @@
+package com.huaguang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassifyHelperServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassifyHelperServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huaguang/ClassifyHelperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huaguang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassifyHelperServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
